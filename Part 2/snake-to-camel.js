ba0b970bb856cf835644b11d5462ab878975e016
kevinreber <kevinreber1@gmail.com>
2020-05-05 14:21:25 -0700

    Part 2

--- Part 2/snake-to-camel.js
+++ Part 2/snake-to-camel.js
@@ -1,2 +1,6 @@
-function snakeToCamel() {}
+function snakeToCamel(snake) {
+    const regex = /([-_]\w)/g;
+    let camel = snake.replace((regex), g => g[1].toUpperCase());
 
+    return camel;
+}
--- Part 2/star-out-grid.js
+++ Part 2/star-out-grid.js
@@ -1,2 +1,38 @@
 function starOutGrid(grid) {
+    const new_grid = [];
+    const star = "*";
+    console.log(grid)
+    for (let row of grid) {
+        // create rows for new_grid
+        if (row.includes(star)) {
+            changeRowToStar(row, star, new_grid);
+        } else new_grid.push(row);
+    }
+    for (let row of grid) {
+        // replace columns gor new_grid
+        for (let [i, el] of row.entries()) {
+            if (el === "*") {
+                changeColumnToStar(i, new_grid, star);
+            }
+        }
+    }
+    return new_grid;
+}
+
+function changeRowToStar(row, star, grid) {
+    // Adds row of stars into grid
+    new_row = [];
+    for (let el of row) {
+        new_row.push(star);
+    }
+    grid.push(new_row);
+    return grid;
+}
+
+function changeColumnToStar(index, grid, star) {
+    // Replaces column of grid with stars
+    for (let row of grid) {
+        row.splice(index, 1, star);
+    }
+    return grid;
 }
