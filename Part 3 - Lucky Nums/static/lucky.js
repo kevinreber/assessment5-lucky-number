f2008ea9b36700b3a68500879d2dd6b3b53c6624
kevinreber <kevinreber1@gmail.com>
2020-05-06 11:55:08 -0700

    set up form handle

--- Part 3 - Lucky Nums/static/lucky.js
+++ Part 3 - Lucky Nums/static/lucky.js
@@ -1,11 +1,21 @@
 /** processForm: get data from form and make AJAX call to our API. */
 
 function processForm(evt) {
+    evt.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+    const email = document.getElementById("email").value;
+    const color = document.getElementById("color").value;
+
+
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
 function handleResponse(resp) {
+    const results = document.getElementById("lucky-resutls");
+
 }
 
 
