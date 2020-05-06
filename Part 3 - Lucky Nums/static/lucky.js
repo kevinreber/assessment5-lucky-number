8791e267da68ac80926765b724c13c00d0ff27b9
kevinreber <kevinreber1@gmail.com>
2020-05-06 12:18:06 -0700

    script: returns response from JSON and returns resutls to user

--- Part 3 - Lucky Nums/static/lucky.js
+++ Part 3 - Lucky Nums/static/lucky.js
@@ -1,21 +1,38 @@
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
+async function processForm(evt) {
     evt.preventDefault();
+    const API_URL = "/api/get-lucky-num";
 
     const name = document.getElementById("name").value;
     const year = document.getElementById("year").value;
     const email = document.getElementById("email").value;
     const color = document.getElementById("color").value;
 
+    // POST request to API
+    const resp = await axios.post(API_URL, {
+        name,
+        year,
+        email,
+        color
+    });
 
+    // store response data and pass to handler
+    const data = resp.data;
+    handleResponse(data);
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
 function handleResponse(resp) {
-    const results = document.getElementById("lucky-resutls");
+    const results = document.getElementById("lucky-results");
 
+    let html = `
+    <p>Your lucky number is ${resp.num.num} (${resp.num.fact}).</p>
+    <p>Your birth year (${resp.year.year}) fact is ${resp.year.fact}.</p>
+    `;
+
+    results.innerHTML = html;
 }
 
 
